0d72daf093c0ebbfe5a66b81cbacfa10f2bcb90d
atlas <atlas@byatlas.com>
2015-07-27 23:03:58 +1200

    Added some more content

--- static/js/leaving.js
+++ static/js/leaving.js
@@ -1,25 +1,42 @@
 var React = React || null // Supress std js error messages
 
-var possibleStates = {
+var introStates = {
   start: {
     content: function () {
       return React.createElement('p', {}, 'And you have to write a farewell email.')
     },
     currentOptions: function () {
       return [
-        { name: 'Start writing the email', nextState: possibleStates.startedWriting },
-        { name: 'Procrastinate for a while', nextState: possibleStates.procrastinating }
+        { name: 'Start writing the email', nextState: introStates.startedWriting },
+        { name: 'Procrastinate for a while', nextState: introStates.procrastinating }
       ]
     }
   },
   startedWriting: {
     content: function () {
-      return React.createElement('p', {}, 'You decide it\'s a way better use of your time to make some memes and try to post them on Reddit. After two hours, you have gained 4 karma.')
+      return React.createElement('p', {}, '"Ettique" dictates you should start with some kind of saluation.')
     },
     currentOptions: function () {
-      return [
-        { name: ''}
-      ]
+      return introOptions
+    }
+  },
+  rudeIntro: {
+    content: function () {
+      return React.createElement('div', {}, [
+        React.createElement('blockquote', {}, 'Hello, you band of unmitigated savages,'),
+        React.createElement('p', {}, 'This may have crossed some kind of line somewhere, but it has a veneer of civility. Especially since you managed to avoid saying fuck. Success!')
+      ])
+    },
+    currentOptions: function () {
+      return introParaOptions
+    }
+  },
+  normalIntro: {
+    content: function () {
+      return React.createElement('p', {}, 'Oh come on, do you think I was going to let you get out that easily?')
+    },
+    currentOptions: function () {
+      return introOptions.slice(0, 3)
     }
   },
   procrastinating: {
@@ -32,15 +49,74 @@ var possibleStates = {
     },
     currentOptions: function () {
       return [
-        { name: '', nextState: '' },
-        { name: '', nextState: '' }
+        { name: 'Start writing the email', nextState: introStates.startedWriting },
+        { name: 'Procrastinate some more', nextState: introStates.continuedProcrastinating }
+      ]
+    }
+  },
+  continuedProcrastinating: {
+    content: function () {
+      return React.createElement('p', {}, 'You haven\'t checked facebook in a while... After scrolling through 6 months of news feed, you feel slightly more autistic.')
+    },
+    updateState: function (currentState) {
+      currentState.autism = true
+      return currentState
+    },
+    currentOptions: function () {
+      return [
+        { name: 'Start writing the email already', nextState: introStates.startedWriting },
+        { name: 'Procrastinate even more', nextState: introStates.evenMoreProcrastinating }
+      ]
+    }
+  },
+  evenMoreProcrastinating: {
+    content: function () {
+      return React.createElement('img', { src: 'http://i.imgur.com/ocqq7.gif' })
+    },
+    currentOptions: function () {
+      return [
+        { name: 'Start writing the email already', nextState: introStates.startedWriting },
+        { name: 'Procrastinate more than you thought possible', nextState: introStates.maximumProcrastination }
+      ]
+    }
+  },
+  maximumProcrastination: {
+    content: function () {
+      return React.createElement('p', {}, 'You decide it would be a way better idea to write some sort of web application to do this instead of just writing an email. Unfortunately then the fourth wall opens, and you talk yourself out of it.')
+    },
+    currentOptions: function () {
+      return [
+        { name: 'Start writing the damn email already', nextState: introStates.startedWriting }
       ]
     }
   }
+
   /* inOutlook: {
     content: 'You open Outlook. You have 5435 emails. Someone has posted your email address on Craigslist solicting "An all night bukkake session". You blame Ben.'
   } */
 }
+
+var introOptions = [
+  { name: 'Act rude', nextState: introStates.rudeIntro },
+  { name: 'Try to be funny', nextState: introStates.funnyIntro },
+  { name: 'Attempt to behave normally', nextState: introStates.normalishIntro },
+  { name: 'Actually behave normally', nextState: introStates.normalIntro }
+]
+
+var paraStates = {
+  joke: {
+
+  },
+  ranting: {
+    content: function () {
+      return React.createElement('p', {}, '')
+    }
+  }
+}
+
+var introParaOptions = [
+  { name: 'Try to warm them up with a joke', nextState: paraStates.joke }
+]
 
 var content = React.createClass({
   render: function () {
@@ -75,7 +151,7 @@ var options = React.createClass({
 var app = React.createClass({
   getInitialState: function () {
     return {
-      current: possibleStates.start,
+      current: introStates.start,
       history: [
       ],
       karma: Math.round(Math.random() * 400 + 80)
