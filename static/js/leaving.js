81d0e368c9f2f3c2bd6a5ab738af51f27d71a448
Liam <liam@ubiquity.co.nz>
2015-07-28 17:18:59 +1200

    Added a whole bunch of content to the leaving page. Still needs more though.

--- static/js/leaving.js
+++ static/js/leaving.js
@@ -38,6 +38,13 @@ var introStates = {
         React.createElement('p', {}, 'Russian is funny, right?'),
         React.createElement('p', {}, 'Better check with a Vlad later...')
       ])
+    },
+    updateState: function (currentState) {
+      currentState.russian = true
+      return currentState
+    },
+    currentOptions: function () {
+      return introParaOptions
     }
   },
   normalishIntro: {
@@ -77,10 +84,10 @@ var introStates = {
   },
   continuedProcrastinating: {
     content: function () {
-      return React.createElement('p', {}, 'You haven\'t checked facebook in a while... After scrolling through 6 months of news feed, you feel slightly more autistic.')
+      return React.createElement('p', {}, 'You haven\'t checked facebook in a while... After scrolling through 6 months of news feed, you feel slightly more loathing torwards everything around you.')
     },
     updateState: function (currentState) {
-      currentState.autism = true
+      currentState.loathing = true
       return currentState
     },
     currentOptions: function () {
@@ -124,9 +131,155 @@ var introOptions = [
   { name: 'Actually behave normally', nextState: introStates.normalIntro }
 ]
 
-var paraStates = {
+var introParaStates = {
   joke: {
+    content: function () {
+      return React.createElement('div', {}, [
+        React.createElement('p', {}, 'You decide the best possible approach is to channel the ghost of Nico\'s past, and write something "punny"'),
+        React.createElement('blockquote', {}, 'I\'d tell you I had an average time here, but that would just be mean.'),
+        React.createElement('p', {}, 'Reading over your masterwork of humor makes you feel terrible, so you know it\'s totally on point')
+      ])
+    },
+    currentOptions: function () {
+      return [
+        { name: 'Try and soothe this awful feeling with a bear', nextState: introParaStates.morePuns },
+        { name: 'MOAR PUNS', nextState: introParaStates.morePuns }
+      ]
+    }
+  },
+  morePuns: {
+    content: function () {
+      return React.createElement('div', {}, [
+        React.createElement('img', { src: 'http://40.media.tumblr.com/tumblr_mb720wBOiQ1ql2603o1_500.jpg'})
+      ])
+    },
+    currentOptions: function () {
+      return [
+        { name: 'Try and soothe this awful feeling with a beer', nextState: introParaStates.punnyBeer },
+        { name: 'Follow up on the puns with some actual words', nextState: bodyParaStates.start }
+      ]
+    }
+  },
+  feels: {
+    content: function () {
+      return React.createElement('div', {}, [
+        React.createElement('p', {}, 'The best way to convince everyone you\'re not some kind of code robot fueled by beer and coffee is to express some sort of emotion in words'),
+        React.createElement('blockquote', {}, 'Well, first of all, I\'d like to thank all of you. It\'s been a real pleasure working with each an everyone of you, especially since by and large you\'ve shown yourselves to be smarter than the clients.'),
+        React.createElement('p', {}, 'The witty bit about the clients is a really nice touch. You high five yourself in your brain.')
+      ])
+    },
+    currentOptions: function () {
+      return [
+        { name: 'All these emotions are overwhelming you. Try and sooth your feelings with a beer', nextState: introParaStates.sadBeer }
+      ]
+    }
+  },
+  beer: {
+    content: function () {
+      return React.createElement('p', {}, 'You decide it\'s time to reward all your hard work with a delicious cold adult beverage.')
+    },
+    updateState: function (currentState) {
+      currentState.beers++
+      return currentState
+    },
+    currentOptions: function () {
+      return introParaOptions.slice(0, 2)
+    }
+  },
+  punnyBeer: {
+    content: function () {
+      return React.createElement('div', {}, [
+        React.createElement('p', {}, 'You open the fridge and collect a bear. Unfortunately, when you attempt to open it, it violently objects and mauls you to death. Worst last day ever.')
+      ])
+    },
+    updateState: summarize,
+    currentOptions: function () {
+      return []
+    }
+  }
+}
 
+var introParaOptions = [
+  { name: 'Try to warm them up with a joke', nextState: introParaStates.joke },
+  { name: 'Go straight for the feels', nextState: introParaStates.feels },
+  { name: 'Reward all your hard work with a beer', nextState: introParaStates.beer }
+]
+
+var bodyParaStates = {
+  start: {
+    content: function () {
+      return React.createElement('p', {}, 'Time to spill some serious ink. You rack your brains trying to think of ways to use your words...')
+    },
+    currentOptions: function () {
+      return [
+        { name: 'Attempt to convey that you learnt well good in your time here', nextState: bodyParaStates.learnt },
+        { name: 'Deploy a tactical cat picture', nextState: bodyParaStates.catPic },
+        { name: 'Offer a guest dog power ranking', nextState: bodyParaStates.dogRating },
+        { name: 'Drop some beats', nextState: bodyParaStates.bassDrop },
+        { name: 'Comment on the sick levels of humor', nextState: bodyParaStates.humor}
+      ]
+    }
+  },
+  humor: {
+
+  },
+  learnt: {
+    content: function () {
+      return React.createElement('blockquote', {}, 'I\'ve managed to pick a lot in my time here, so thanks for giving me the chance to do that. Some of it two or three times, even. Hopefully I never have to learn how code emails ever again. It\'s been nice working with some more or less sane developers for a while. Hopefully at the next gig they\'re as on point as they seem.')
+    },
+    currentOptions: function () {
+      return [
+        { name: 'Deploy a tactical cat picture', nextState: bodyParaStates.catPic },
+        { name: 'Offer a guest dog power ranking', nextState: bodyParaStates.dogRating }
+      ]
+    }
+  },
+  dogRating: {
+    content: function () {
+      return React.createElement('blockquote', {}, [
+          'I think it\'s super important that you all know which dog is the best dog.',
+          React.createElement('ol', {}, [
+            React.createElement('li', {}, 'Steve (Points for having a people name, and scaring the courier that one time)'),
+            React.createElement('li', {}, 'Spirit (Mad props for annoying Ben)'),
+            React.createElement('li', {}, 'Fliss (SO MUCH FLUFFY)'),
+            React.createElement('li', {}, 'Molly (Cuter than Leela. Probably because she\'s smaller)'),
+            React.createElement('li', {}, 'Leela (See above)'),
+            React.createElement('li', {}, 'Nacho (Bitey)')
+          ]),
+          'Incidentally, I think they\'re ordered by size. Or close to it. Totally nothing to do with me not being a fan of handbag dogs.'
+        ])
+    },
+    currentOptions: function () {
+      return [
+        { name: 'Attempt to convey that you learnt well good in your time here', nextState: bodyParaStates.learnt },
+        { name: 'Deploy a tactical cat picture', nextState: bodyParaStates.catPic },
+        { name: 'Offer a guest dog power ranking', nextState: bodyParaStates.dogRating },
+        { name: 'Drop some beats', nextState: bodyParaStates.bassDrop }
+      ]
+    }
+  },
+  bassDrop: {
+    content: function () {
+      return React.createElement('div', {}, [
+        React.createElement('p', {}, 'You\'re really impressed that you managed to find a way to jam audio in an "Email".'),
+        React.createElement('iframe', { width: '100%', height: '450', scrolling: 'no', frameborder: 'no', src: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/45959360&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'})
+      ])
+    },
+    currentOptions: function () {
+      return [
+        { name: 'Attempt to convey that you learnt well good in your time here', nextState: bodyParaStates.learnt }
+      ]
+    }
+  },
+  catPic: {
+    content: function () {
+      return React.createElement('img', { src: 'http://i.imgur.com/8ByaI.gif' })
+    },
+    currentOptions: function () {
+      return [
+        { name: 'Attempt to convey that you learnt well good in your time here', nextState: bodyParaStates.learnt }
+      ]
+    }
   },
   ranting: {
     content: function () {
@@ -135,11 +288,10 @@ var paraStates = {
   }
 }
 
-var introParaOptions = [
-  { name: 'Try to warm them up with a joke', nextState: paraStates.joke },
-  { name: 'Go straight for the feels', nextState: paraStates.feels }
-
-]
+function summarize (currentState) {
+  currentState.summary = true
+  return currentState
+}
 
 var content = React.createClass({
   render: function () {
@@ -171,13 +323,49 @@ var options = React.createClass({
   }
 })
 
+var summary = React.createClass({
+  render: function () {
+    var children = []
+
+    children.push(React.createElement('h1', {}, 'GAME OVER'))
+
+    if (this.props.completedEmail === true) {
+      children.push(React.createElement('img', { src: 'http://i.imgur.com/uLj0u.gif' }))
+      children.push(React.createElement('p', {}, 'Against all odds, you managed to finish the email. Your colleagues will be forever thankful for your wisdom.'))
+    } else {
+      children.push(React.createElement('img', { src: 'http://i.imgur.com/Gn04H.gif' }))
+      children.push(React.createElement('p', {}, 'You failed in your god given task of sending a farewell email. Your legacy is destined to be forgotten.'))
+    }
+
+    if (this.props.beers > 0) {
+      children.push(React.createElement('p', {}, 'You successfully consumed ' + this.props.beers + ' beers'))
+    }
+
+    if (this.props.russian === true) {
+      children.push(React.createElement('p', {}, 'You managed to "master" the Russian language'))
+    }
+
+    if (this.props.autism === true) {
+      children.push(React.createElement('p', {}, 'You somehow managed to contract autism. You suspect the air conditioning is somehow involved'))
+    }
+
+    return React.createElement('div', { className: 'summary' }, children)
+  }
+})
+
 var app = React.createClass({
   getInitialState: function () {
     return {
       current: introStates.start,
       history: [
       ],
-      karma: Math.round(Math.random() * 400 + 80)
+      karma: Math.round(Math.random() * 400 + 80),
+      russian: false,
+      loathing: false,
+      beers: 0,
+      summary: false,
+      completedEmail: false,
+      autism: false
     }
   },
   optionHandler: function (current, clickedOption) {
@@ -204,9 +392,22 @@ var app = React.createClass({
     state.optionHandler = this.optionHandler
 
     children.push(React.createElement(content, this.state))
-    children.push(React.createElement(options, state))
+    if (this.state.summary === false) {
+      children.push(React.createElement(options, state))
+    }
 
-    return React.createElement('div', {}, children)
+    var css = {}
+
+    if (this.state.beers > 0) {
+      css.color = 'transparent'
+      css.textShadow = '0 0 ' + this.state.beers + 'px rgba(0, 0, 0, 0.75)'
+    }
+
+    if (this.state.summary === true) {
+      children.push(React.createElement(summary, this.state))
+    }
+
+    return React.createElement('div', { style: css }, children)
   }
 })
 
